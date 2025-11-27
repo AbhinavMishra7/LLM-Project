74bba6f5c10ce172c1c94e271a7a97ccdf61230c
Abhinav Mishra <abhinavmishra@Abhinavs-MacBook-Air.local>
2025-11-27 15:21:38 +0530

    Latest updates

--- Tokenization.py
+++ Tokenization.py
@@ -1,4 +1,5 @@
 import re
+from Tokenizer_V1 import SimpleTokenizerV1 
 
 with open ("LLM-Project/harrypotter.txt","r",encoding="utf-8") as f:
     raw_text=f.read()
@@ -10,13 +11,23 @@ preprocessed=re.split (r'([,.:;?-_!"()\']|--|\s)',raw_text)
 preprocessed=[item for item in preprocessed if item.strip()]
 print(preprocessed[:30])
 
-all_words=sorted(set(preprocessed))
+all_words=sorted(list(set(preprocessed)))
 vocab_size=len(all_words)
-
-print(vocab_size)
+all_words.extend(["<|endoftext|>","<|unk|"])
 vocab ={token:integer for integer,token in enumerate(all_words)}
+
+
 
 for i, item in enumerate(vocab.items()):
     print(item)
     if i>=200:
         break
+
+tokenizer=SimpleTokenizerV1(vocab)
+text='''He is a man.'''
+text2='''harry is <|endoftext|>'''
+ids=tokenizer.encode(text2)
+print(ids)
+
+text1=tokenizer.decode(ids)
+print(text1)
